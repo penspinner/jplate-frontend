d47d4cb80bccb9690ffe960d8112208badf6cb88
Josh Melo <joshuamelo1126@gmail.com>
2020-10-02 00:46:50 -0400

    added about stuff

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,14 @@ const Footer = () => {
     <div className="bg-black w-full h-32 absolute bottom-0 grid gap-4 grid-cols-3 items-center">
       <span className="flex justify-center">
         <Link href="https://github.com/mallockey">
-          <img src="./GitHub-Mark-Light-64px.png" />
+          <img className="cursor-pointer" src="./GitHub-Mark-Light-64px.png" />
         </Link>
       </span>
-      <span className="text-center text-white">Social </span>
+      <span className="flex justify-center">
+        <Link href="https://www.linkedin.com/in/joshua-melo/">
+          <img className="cursor-pointer" src="./linkedin.svg" style={{ height: '50px' }}></img>
+        </Link>
+      </span>
       <span className="text-center text-white">Stack</span>
     </div>
   )
