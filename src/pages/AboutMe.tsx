c0df1b95247e43b84af643084ec2b647b3c50a01
Amy Liao <45218453+ogbanana@users.noreply.github.com>
2020-10-11 19:01:53 -0400

    mobile friendly (#7)
    * mobile friendly
    
    * prettier check

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -6,20 +6,23 @@ const About = () => {
   return (
     <>
       <Header />
-      <div className="flex items-center flex-wrap md:flex-no-wrap justify-around pb-4 pt-4 h-screen">
+      <div className="flex items-center flex-wrap md:flex-no-wrap justify-around h-full">
+        <div className="sm: w-full lg:w-2/4 md:text-xl">
+          <h1 className=" text-center font-bold text-2xl pb-5 underline ">About me</h1>
+          <p className="pl-6 pr-6 text-lg">
+            {`Hello there, my names Josh! I love learning all things computers and computer
+            software. My latest endevor has taken me down the path of web design using JavaScript,
+            Node, Express, React, TailwindCSS. I also have extensive experience with system
+            engineering and automation/scripting using Windows PowerShell. When I'm not writing
+            code, I enjoy playing drums/guitar, reading books about the brain and exploring new
+            cities.`}
+          </p>
+        </div>
         <img
           src="/joshdrum.jpg"
-          className="rounded-lg"
+          className="m-4 rounded-lg"
           style={{ height: '30rem', width: 'auto' }}
         />
-        <div className="sm: w-full sm: text-xs text-center lg:w-2/4 md:text-xl">
-          <h1 className=" text-center font-bold text-2xl pb-10 underline ">About me</h1>
-          Hello there, my names Josh! I love learning all things computers and computer software. My
-          latest endevor has taken me down the path of web design using JavaScript, Node, Express,
-          React, TailwindCSS. I also have extensive experience with system engineering and
-          automation/scripting using Windows PowerShell. When I'm not writing code, I enjoy playing
-          drums/guitar, reading books about the brain and exploring new cities.
-        </div>
       </div>
       <Footer />
     </>
--- src/pages/WhatIsThis.tsx
+++ src/pages/WhatIsThis.tsx
@@ -3,48 +3,40 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 const WhatIsThis = () => {
+  const technologyUsed = [
+    'React Hooks JS',
+    'Next JS',
+    'Tailwind CSS',
+    'PostgreSQL',
+    'Heroku',
+    'TypeScript',
+    'Webpack',
+  ]
   return (
     <div>
       <Header />
       <div className="w-auto text-left h-screen flex-col items-center justify-center ">
         <h1 className="text-center underline pb-8 pt-16">What is JPlate?</h1>
-        <article className="text-center pb-8">
+        <article className="text-center pb-8 pr-4 pl-4">
           JPlate is a mock online store that my team and I created to gain experience with the
           following technologies:
         </article>
         <div className="flex justify-center">
-          <table className="flex-col justify-center text-left ">
-            <tr>
-              <td className="text-green-400">&#10003;</td>
-              <td>React Hooks JS</td>
-            </tr>
-            <tr>
-              <td className="text-green-400">&#10003;</td>
-              <td>Next JS</td>
-            </tr>
-            <tr>
-              <td className="text-green-400">&#10003;</td>
-              <td>Tailwind CSS </td>
-            </tr>
-            <tr>
-              <td className="text-green-400">&#10003;</td>
-              <td>PostgreSQL </td>
-            </tr>
-            <tr>
-              <td className="text-green-400">&#10003;</td>
-              <td>Heroku</td>
-            </tr>
-            <tr>
-              <td className="text-green-400">&#10003;</td>
-              <td>TypeScript </td>
-            </tr>
-            <tr>
-              <td className="text-green-400">&#10003;</td>
-              <td>Webpack </td>
-            </tr>
+          <table>
+            <tbody className="text-center">
+              <tr>
+                {technologyUsed.map((item) => {
+                  return (
+                    <td key={Math.random()} className="flex text-left">
+                      <span className="text-green-400">&#10003;</span> {item}
+                    </td>
+                  )
+                })}
+              </tr>
+            </tbody>
           </table>
         </div>
-        <article className="pt-16 text-center">
+        <article className="pt-16 pr-4 pl-4 text-center">
           JPlate is meant to be an imitation online store. This current version has the cart
           functionality. In the next version we will implement the sign in/sign up and checkout
           functionalities.
