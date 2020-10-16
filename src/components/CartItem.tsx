472221a4700872b4569b6ec254e1dc5e7dcef3fb
Amy Liao <a.liao.uey@gmail.com>
2020-10-16 00:44:07 -0400

    screen height and plates center

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -8,7 +8,7 @@ const CartItem = (props: {
   handleSetCart: (arg0: any) => void
 }) => {
   return (
-    <div className="hover:bg-gray-200 rounded-lg pt-5 pb-5 w-full flex flex-col md:grid md:grid-flow-row md:grid-cols-3 md:gap-5 md:pr-5 md:pl-5 md:pb-5 md:pt-5 md:justify-center md:items-center">
+    <div className="hover:bg-gray-200 rounded-lg pt-5 pb-5 w-full flex flex-col md:grid md:grid-flow-row md:grid-cols-3 md:gap-5 md:pr-5 md:pl-5 md:pb-5 md:pt-5 md:justify-center items-center">
       <span>
         <img className="h-56 w-64" src={props.item.imageURL} />
       </span>
