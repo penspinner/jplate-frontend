af0dd9a6a9e990668d0ead44431dc910f1d89873
Amy Liao <45218453+ogbanana@users.noreply.github.com>
2020-10-16 00:36:56 -0400

    Mobile cart (#10)
    * mobile friendly
    
    * merge conflicts
    
    * hamburger menu has cart
    
    * empty cart mobile responsive
    
    * mobile cart responsiveness style
    
    * remove packagejson and fixed merge conflicts
    
    * new image

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -8,12 +8,12 @@ const CartItem = (props: {
   handleSetCart: (arg0: any) => void
 }) => {
   return (
-    <div className="w-full grid grid-flow-row grid-cols-3 gap-5 pr-5 pl-5 pb-5 pt-5 hover:bg-gray-200 rounded-lg justify-items-center items-center">
+    <div className="hover:bg-gray-200 rounded-lg pt-5 pb-5 w-full flex flex-col md:grid md:grid-flow-row md:grid-cols-3 md:gap-5 md:pr-5 md:pl-5 md:pb-5 md:pt-5 md:justify-center md:items-center">
       <span>
         <img className="h-56 w-64" src={props.item.imageURL} />
       </span>
 
-      <span className="flex flex-col justify-center items-center">
+      <span className="md:flex md:flex-col md:justify-center md:items-center">
         <div className="text-xl font-bold text-center">{props.item.name}</div>
         <div className=" pt-2 text-md">
           Price per plate: <span className="text-lg">${props.item.price}</span>
@@ -24,7 +24,7 @@ const CartItem = (props: {
         <button
           type="button"
           id="removeFromCart"
-          className="static right-0 bg-red-300 hover:bg-red-500 p-2 rounded-lg"
+          className="md:static md:right-0 bg-red-300 hover:bg-red-500 md:p-2 ml-48 rounded-lg p-1"
           onClick={() => {
             const newCart = removeFromCart(props.cart, props.item)
             props.handleSetCart(newCart)
